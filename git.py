54b2169a95450f14d82ae4184a8ecd44151f4d32
Atnatewos <icodeyt21@gmail.com>
2026-04-22 11:42:27 -0700

    add the commit revert method guide

--- git.py
+++ git.py
@@ -67,3 +67,6 @@ print(diffChanges)
 # for a tracked file (a file that's been commited before we can skip the staging stage if we just want to commit all of it)
 # but this won't work for a new created file since that's untracked, and this will only work for a file that's been commited before
 # to do that we add the `a-flag` when commiting, 'subprocess.run(["git", "commit", "-a", "-m", "this is the change where i skipped staging"], text=True)'
+
+# What if we want to revert it after we commited it, then we'd use the `subprocess.run(["git", "revert", "HEAD (the latest commit)"], text=True)
+#
