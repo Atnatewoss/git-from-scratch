06974ac112e7143b5f3d57e5b832e7ed65280774
Atnatewos <icodeyt21@gmail.com>
2026-04-23 01:29:08 -0700

    add merge-conflict setups

--- git_tools.py
+++ git_tools.py
@@ -29,12 +29,16 @@ def manage_branch_workflow():
         pass
 
     def merge_branch():
+        merge_conflict = False
         types_of_merges = ["fast forward", "three way merge"]
         # three way merge: could happen if we create a commit on the main branch after creating another branch
         # if the changes were made in different files, the git is good to go.
         # but if they were made in the same file, git will take both of the changes and put them in a result.
         # but instead if the changes were made on the same part in the same file, git won't know how to merge those changes, and the attempt will result in a merge conflict
         pass
+
+        if merge_conflict:
+            pass
 
 
 def config_git_user(user_email, user_name):
