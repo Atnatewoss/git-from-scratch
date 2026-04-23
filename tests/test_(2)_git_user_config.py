5f8c5b0bc45330b021d2e81a394d274ff02849d7
atnatewoss <icodeyt21@gmail.com>
2026-04-23 04:32:27 -0700

    add the user_config test back
    add the git_user_config test back without activating the function as that changed my git username whilst testing it

--- tests/test_(2)_git_user_config.py
+++ tests/test_(2)_git_user_config.py
@@ -5,8 +5,8 @@ class Git:
     ### --- SETUP INPUTS --- ###
 
     # B) user config info
-    # user_email = "mathew13@gmail.com"
-    # user_name = "mathew"
+    user_email = "mathew13@gmail.com"
+    user_name = "mathew"
 
     # ### --- EXECUTE FUNCTION TOOLS --- ###
     # config_git_user = config_git_user(user_email, user_name)
