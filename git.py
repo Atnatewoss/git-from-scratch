6cfd217940a56508d755ea983e914e6c8796eabc
atnatewoss <icodeyt21@gmail.com>
2026-04-24 11:28:28 -0700

    refactor(git): refactor the setups into their own git_config.py file

--- git.py
+++ git.py
@@ -1,3 +1,18 @@
+from git_config import (
+    action,
+    committed,
+    current_branch,
+    default_branch,
+    list_of_branches,
+    new_branch,
+    new_file,
+    old_file,
+    repo_name,
+    staged,
+    user_email,
+    user_name,
+    workflow_tree,
+)
 from git_tools import (
     config_git_user,
     diff,
@@ -8,42 +23,7 @@ from git_tools import (
 
 
 class Git:
-    ### --- SETUP INPUTS --- ###
-    # A) workflow process management
-    workflow_tree = True
-    staged = True
-    committed = True
-
-    # branch workflow setup
-    default_branch = "main"
-    list_of_branches = [default_branch]
-
-    # new branch
-    new_branch = "new branch"
-
-    if not new_branch:
-        current_branch = default_branch
-
-    current_branch = new_branch
-
-    # diff setup
-    old_file = "old file.py, for now"
-    new_file = "new file.py, for now"
-
-    # B) user config info
-    user_email = "user_email"
-    user_name = "user_name"
-
-    # C) repo name
-    repo_name = "new repo"
-
-    # D) list all of the commmands used in the tools here, to track them as a check list
-    list_of_commands = []
-
     ### --- EXECUTE FUNCTION TOOLS --- ###
-    # what tasks we would like git to perform
-    # create, delete, change, merge
-    action = ""
     manage_branch_workflow = manage_branch_workflow(
         default_branch, list_of_branches, current_branch, new_branch, action
     )
